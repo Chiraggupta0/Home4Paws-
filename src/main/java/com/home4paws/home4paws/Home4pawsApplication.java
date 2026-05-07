5d7fe410fbe6b11980590308e8e7c4af4b706aa3
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2026-05-07 15:09:10 +0530

    day 3

---
+++ src/main/java/com/home4paws/home4paws/Home4pawsApplication.java
@@ -0,0 +1,13 @@
+package com.home4paws.home4paws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home4pawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home4pawsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/home4paws/home4paws/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.home4paws.home4paws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+    @GetMapping("/c")
+    public String err() {
+        return "Hello from wev !";
+    }
+}
